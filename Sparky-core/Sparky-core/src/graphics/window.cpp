ab5806ba7e18a801dc64e490149ee7da140319e8
filipegrilo <filipe.r.grilo@ua.pt>
2015-06-08 21:51:15 +0100

    Vertex Array, Buffer and Index Buffers added

--- Sparky-core/Sparky-core/src/graphics/window.cpp
+++ Sparky-core/Sparky-core/src/graphics/window.cpp
@@ -70,6 +70,10 @@ namespace sparky{ namespace graphics{
 	}
 
 	void Window::update(){
+		GLenum error = glGetError();
+		if (error != GL_NO_ERROR)
+			std::cout << "OpenGL Error: " << error << std::endl;
+
 		glfwPollEvents();
 		glfwSwapBuffers(m_Window);
 	}
